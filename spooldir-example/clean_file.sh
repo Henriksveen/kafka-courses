7defe745c0af1e1c2818be239e6c27e7756668e8
Henriksveen <simen.henriksveen@safebase.no>
2020-02-18 13:41:23 +0100

    Update help-scripts

--- spooldir-example/clean_file.sh
+++ spooldir-example/clean_file.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 ## Delete all lines starting with given words for each file
-find -maxdepth 2 -type f -name '*.dat' -exec sed -i "/\b\(^DOMAIN\|^TABLE\|^VERSION\|^PERIODSTART\|^PERIODEND\|^SEQNO\|^RECORDCOUNT\)\b/d" {} \;
+find /example/spooldir/ -maxdepth 2 -type f -name '*.dat' -exec sed -i "/\b\(^DOMAIN\|^TABLE\|^VERSION\|^PERIODSTART\|^PERIODEND\|^SEQNO\|^RECORDCOUNT\)\b/d" {} \;
 
 ## Replace all NULL with empty string for each file
-find -maxdepth 2 -type f -name '*.dat' -exec sed -i 's/NULL//g' {} \;
-
-mv finished/* data
+find /example/spooldir/ -maxdepth 2 -type f -name '*.dat' -exec sed -i 's/NULL//g' {} \;
--- spooldir-example/setup.sh
+++ spooldir-example/setup.sh
@@ -6,6 +6,12 @@ echo "Create topic nobill-call-record-hour"
 kafka-topics --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 3 --topic nobill-call-record-hour
 echo "Create topic nobill-call-record-day" 
 kafka-topics --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 3 --topic nobill-call-record-day
+echo "Create topic nobill-sms-record" 
+kafka-topics --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 3 --topic nobill-sms-record
+echo "Create topic nobill-sms-record-hour" 
+kafka-topics --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 3 --topic nobill-sms-record-hour
+echo "Create topic nobill-sms-record-day" 
+kafka-topics --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 3 --topic nobill-sms-record-day
 
 
 echo "List all topics" 
